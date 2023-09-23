2d3ef70c7c448b1c178cc8ca14298f20301d9f1b
jawwaonline <jawwa@jawwa.de>
2023-09-23 20:00:57 +0200

    Transforming to use Array of Images through props.
    Adding right left arrow.
    
    Width as default and changeable by props

--- 22_slider/src/App.jsx
+++ 22_slider/src/App.jsx
@@ -1,10 +1,29 @@
 import { Slider } from './components/Slider';
 
 function App() {
+  const images = [
+    {
+      url: 'https://imgs.search.brave.com/9FQZpcYQGpsHf5RaEUEAczJ3aX4jgH1o7NuHn8HA8TQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzg2LzM3Lzgz/LzM2MF9GXzg2Mzc4/MzMyX2VKempXY2sw/S213aUhJUTNpakpS/Z21IQXBMV2dBWFFl/LmpwZw',
+      title: 'image of sunset'
+    },
+    {
+      url: 'https://imgs.search.brave.com/Mpd2Arapsq0uCy1q2JfRwKEWDnKHlKeDVDGUrRS0xx0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9z/a3ktc3Vuc2V0XzEy/MDMtMTY2Ni5qcGc_/c2l6ZT02MjYmZXh0/PWpwZw',
+      title: 'image of sunset 2'
+    },
+    {
+      url: 'https://imgs.search.brave.com/BGrHXJV19tibCbtbGliZsJAHRSSezJEqaK663ld6zq4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTI1/MDE4NTUzL3Bob3Rv/L3NreS1iYWNrZ3Jv/dW5kLW9uLXN1bnNl/dC5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9UVlHYWNDeWNx/OHVwSWx1clJoS0ZX/M2lIWG42eW51YmZf/b1d5ZFBrcGd4ND0',
+      title: 'image of sunset 2'
+    }
+  ];
+
   return (
     <>
-      <h2 className="text-red-600">Creating a Slider Component</h2>
-      <Slider></Slider>
+      <main className="flex flex-col place-items-center align-middle bg-slate-400 min-w-screen min-h-screen">
+        <h2 className="text-red-600 font-bold text-3xl my-4">
+          Creating a Slider Component
+        </h2>
+        <Slider images={images} />
+      </main>
     </>
   );
 }
--- 22_slider/src/components/Slider.jsx
+++ 22_slider/src/components/Slider.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
 
-export function Slider() {
+export function Slider({ images, width }) {
+  // TODO TYPEDEFINITION IMAGES ARRAY
+
   const [counter, setcounter] = useState(0);
 
-  function moveonclick() {
-    if (counter == 2) {
+  function moverightonclick() {
+    if (counter == images?.length - 1) {
       setcounter(0);
       return;
     }
     setcounter(counter + 1);
-    console.log(counter);
+  }
+
+  function moveleftonclick() {
+    if (counter == 0) {
+      setcounter(2);
+      return;
+    }
+    setcounter(counter - 1);
   }
 
   const position = {
@@ -18,37 +27,36 @@ export function Slider() {
 
   return (
     <>
-      <section className="bg-slate-300 h-96 w-full mt-40 flex flex-col place-items-center">
-        <h1>Slider Component</h1>
-
+      <section
+        className={`slider-wrapper flex justify-center outline-4 outline-dotted relative outline-zinc-700 overflow-hidden ${
+          width ? `max-w-[${width}]` : 'max-w-[300px]'
+        }`}
+      >
         <div
-          onClick={moveonclick}
-          className="slider-wrapper flex outline-4 outline-dotted outline-zinc-700 w-1/2 h-full overflow-hidden"
+          onClick={moveleftonclick}
+          className="my-auto text-4xl absolute z-10 self-center cursor-pointer left-0 "
         >
-          {' '}
-          <div className="my-auto bg-red-300 absolute z-10 self-center ">
-            arrowleft
-          </div>
-          <div
-            style={position}
-            className="holding images flex ease-in-out duration-500 transition"
-          >
-            <img
-              className={`object-cover `}
-              alt="image of sunset"
-              src="https://imgs.search.brave.com/9FQZpcYQGpsHf5RaEUEAczJ3aX4jgH1o7NuHn8HA8TQ/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzg2LzM3Lzgz/LzM2MF9GXzg2Mzc4/MzMyX2VKempXY2sw/S213aUhJUTNpakpS/Z21IQXBMV2dBWFFl/LmpwZw"
-            />
-            <img
-              className={`object-cover `}
-              alt="image of sunset"
-              src="https://imgs.search.brave.com/Mpd2Arapsq0uCy1q2JfRwKEWDnKHlKeDVDGUrRS0xx0/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9z/a3ktc3Vuc2V0XzEy/MDMtMTY2Ni5qcGc_/c2l6ZT02MjYmZXh0/PWpwZw"
-            />
-            <img
-              className={`object-cover `}
-              alt="image of sunset"
-              src="https://imgs.search.brave.com/BGrHXJV19tibCbtbGliZsJAHRSSezJEqaK663ld6zq4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNTI1/MDE4NTUzL3Bob3Rv/L3NreS1iYWNrZ3Jv/dW5kLW9uLXN1bnNl/dC5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9UVlHYWNDeWNx/OHVwSWx1clJoS0ZX/M2lIWG42eW51YmZf/b1d5ZFBrcGd4ND0"
-            />
-          </div>
+          ⬅
+        </div>
+        <div
+          onClick={moverightonclick}
+          className="my-auto text-4xl absolute z-10 self-center cursor-pointer right-0 "
+        >
+          ➡
+        </div>
+        <div
+          style={position}
+          className="image-holder flex ease-in-out duration-500 transition"
+        >
+          {images?.map((image) => {
+            return (
+              <img
+                key={image?.url}
+                src={image?.url}
+                alt={image?.title}
+              />
+            );
+          })}
         </div>
       </section>
     </>
