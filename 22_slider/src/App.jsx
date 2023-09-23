ad3c34e23352ddd2ba6898119066d6ac577a59e5
jawwaonline <jawwa@jawwa.de>
2023-09-23 11:00:11 +0200

    working on slider

---
+++ 22_slider/src/App.jsx
@@ -0,0 +1,12 @@
+import { Slider } from './components/Slider';
+
+function App() {
+  return (
+    <>
+      <h2 className="text-red-600">Creating a Slider Component</h2>
+      <Slider></Slider>
+    </>
+  );
+}
+
+export default App;
