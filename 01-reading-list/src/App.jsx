9cce79ad6369e578f6f6943edcfd4b4f39c8e542
jawwaonline <jawwa@jawwa.de>
2023-10-02 20:45:20 +0200

    added scaling on hover and grid col numbers on smaller devices

--- 01-reading-list/src/App.jsx
+++ 01-reading-list/src/App.jsx
@@ -5,23 +5,27 @@ const Books = BookData();
 export default function App() {
   return (
     <>
-      <h3 className="text-3xl"> Hello World</h3>
-      <div className="grid grid-cols-3 gap-5 absolute">
-        {Books.map((book) => {
-          return (
-            <div
-              className="flex flex-col h-full items-center transition-all "
-              key={book.ISBN}
-            >
-              <p className="relative flex-1 mt-0">{book.title}</p>
-              <img
-                className="h-40 relative object-cover items-center mb-0 rounded-md hover:scale-125 shadow-slate-700 shadow-xl"
-                src={book.cover}
-              />
-            </div>
-          );
-        })}
-      </div>
+      <section className="w-[80%] flex-col mx-auto">
+        <h3 className="text-3xl text-center"> Hello World</h3>
+        <div className="grid md:grid-cols-3 xs:grid-cols-1 sm:grid-cols-2 gap-10 ">
+          {Books.map(({ ISBN, cover, title }) => {
+            return (
+              <div
+                className="flex flex-col h-full items-center transition-all bg-white rounded-lg hover:scale-105 shadow-slate-700 shadow-xl p-5 "
+                key={ISBN}
+              >
+                <img
+                  className="h-40 relative object-cover items-center mb-0 ease-in-out duration-300 hover:scale-110 shadow-slate-700 shadow-xl"
+                  src={cover}
+                />
+                <p className="relative flex-1 text-center mt-6">
+                  {title}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
